b4746324b84b44aa456397e1afe842e85a9701da
Mukesh-coder7 <mukeshbabuacharyaS@gmail.com>
2024-12-10 08:39:43 +0545

    Generalrepository

---
+++ WebApplication1/Repositories/GenericRepository/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using WebApplication1.Data;
+
+namespace WebApplication1.Repositories.GenericRepository
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    #region Ctor & Properties
+
+    private readonly DbSet<T> _dbSet;   
+
+    public Repository(ApplicationDbContext) : base(DbContext)
+    {
+        _dbSet = DbContext.Set<T>();
+    }
+
+    #endregion
+
+}
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509f69344d5420c5a6113349269a89f298d5845d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7eb4904113484ef89dc4ad4059e61a3dd06d77d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
